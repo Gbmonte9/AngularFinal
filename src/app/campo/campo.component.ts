54555955048419259ff12051eaa2a1f479ade780
Gabriel Monte <gabrielmonte485@gmail.com>
2025-02-06 09:50:30 -0300

    Criação de campos dentro do campo.

--- src/app/campo/campo.component.ts
+++ src/app/campo/campo.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FotoComponent } from '../foto/foto.component';
+import { TituloComponent } from '../titulo/titulo.component';
+import { DescricaoComponent } from '../descricao/descricao.component';
+import { CategoriaComponent } from '../categoria/categoria.component';
 
 @Component({
   selector: 'app-campo',
-  imports: [],
+  imports: [FotoComponent, TituloComponent, DescricaoComponent, CategoriaComponent],
   templateUrl: './campo.component.html',
   styleUrl: './campo.component.css'
 })
---
+++ src/app/descricao/descricao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descricao',
+  imports: [],
+  templateUrl: './descricao.component.html',
+  styleUrl: './descricao.component.css'
+})
+export class DescricaoComponent {
+
+}
